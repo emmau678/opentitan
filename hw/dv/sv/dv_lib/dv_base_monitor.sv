emmau678/opentitan

commit 5954b13640d71d92a14dec354d1ada2346aa784e

[dv] make dv_base_agent work for high-level agent
Tried to create a high-level agent but then I found it's not much
different from dv_base_agent. So, I feel it's better to add what we need
for high-level agent in dv_base_agent
1. 2 mon2sqr port/fifo, which can be used for re-active driver. Modified
i2c to use the mon2sqr_req_fifo for re-active driver
2. add knob en_driver for high-level agent which doesn't need a driver

Next step will update uvmdvgen to generate agent with a low-level agent
and connect those monitors

Signed-off-by: Weicai Yang <weicai@google.com>

hw/dv/sv/dv_lib/dv_base_monitor.sv
@@ -20,11 +20,18 @@ class dv_base_monitor #(type ITEM_T = uvm_sequence_item,
   // Analysis port for the collected transfer.
   uvm_analysis_port #(ITEM_T) analysis_port;
 
+  // item will be sent to this port for seq when req phase is done (last is set)
+  uvm_analysis_port #(ITEM_T) req_analysis_port;
+  // item will be sent to this port for seq when rsp phase is done (rsp_done is set)
+  uvm_analysis_port #(ITEM_T) rsp_analysis_port;
+
   `uvm_component_new
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     analysis_port = new("analysis_port", this);
+    req_analysis_port = new("req_analysis_port", this);
+    rsp_analysis_port = new("rsp_analysis_port", this);
   endfunction
 
   virtual task run_phase(uvm_phase phase);

hw/dv/sv/dv_lib/dv_base_sequencer.sv
@@ -11,8 +11,21 @@ class dv_base_sequencer #(type ITEM_T     = uvm_sequence_item,
                                                  .CFG_T      (CFG_T),
                                                  .RSP_ITEM_T (RSP_ITEM_T)))
 
+  // These fifos collects items when req/rsp is received, which are used to communicate between
+  // monitor and sequences. These fifos are optional
+  // When device is re-active, it gets items from req_analysis_fifo and send rsp to driver
+  // When this is a high-level agent, monitors put items to these 2 fifos for high-level seq
+  uvm_tlm_analysis_fifo #(ITEM_T) req_analysis_fifo;
+  uvm_tlm_analysis_fifo #(ITEM_T) rsp_analysis_fifo;
+
   CFG_T cfg;
 
   `uvm_component_new
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if (cfg.has_req_fifo) req_analysis_fifo = new("req_analysis_fifo", this);
+    if (cfg.has_rsp_fifo) rsp_analysis_fifo = new("rsp_analysis_fifo", this);
+  endfunction : build_phase
 
 endclass

hw/dv/sv/csrng_agent/csrng_agent.sv
@@ -40,10 +40,12 @@ class csrng_agent extends dv_base_agent #(
                                    create("m_genbits_push_agent_cfg");
     cfg.m_genbits_push_agent_cfg.is_active  = cfg.is_active;
     cfg.m_genbits_push_agent_cfg.agent_type = PushAgent;
-    if (cfg.if_mode == dv_utils_pkg::Host)
+    if (cfg.if_mode == dv_utils_pkg::Host) begin
+      cfg.has_req_fifo = 1;
       cfg.m_genbits_push_agent_cfg.if_mode = dv_utils_pkg::Device;
-    else
+    end else begin
       cfg.m_genbits_push_agent_cfg.if_mode = dv_utils_pkg::Host;
+    end
 
     cfg.vif.if_mode = cfg.if_mode;
 
@@ -64,7 +66,7 @@ class csrng_agent extends dv_base_agent #(
 
     if (cfg.is_active) begin
       if (cfg.if_mode == dv_utils_pkg::Device) begin
-        monitor.req_port.connect(sequencer.cmd_req_fifo.analysis_export);
+        monitor.req_analysis_port.connect(sequencer.req_analysis_fifo.analysis_export);
         sequencer.m_genbits_push_sequencer = m_genbits_push_agent.sequencer;
       end
     end

hw/dv/sv/csrng_agent/csrng_monitor.sv
@@ -14,20 +14,8 @@ class csrng_monitor extends dv_base_monitor #(
   // csrng_agent_cov: cov
   // uvm_analysis_port #(csrng_item): analysis_port
 
-  // item will be sent to this port when req phase is done (last is set)
-  uvm_analysis_port#(csrng_item) req_port;
-  // item will be sent to this port when rsp phase is done (rsp_done is set)
-  uvm_analysis_port#(csrng_item) rsp_port;
-
   `uvm_component_new
   bit in_reset;
-
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-
-    req_port  = new("req_port", this);
-    rsp_port  = new("rsp_port", this);
-  endfunction
 
   task run_phase(uvm_phase phase);
     @(posedge cfg.vif.rst_n);
@@ -70,7 +58,7 @@ class csrng_monitor extends dv_base_monitor #(
         item = csrng_item::type_id::create("item");
         item.acmd = cfg.vif.mon_cb.cmd_req.csrng_req_bus[3:0];
         `uvm_info(`gfn, $sformatf("Captured item: %s", item.convert2string()), UVM_HIGH)
-        req_port.write(item);
+        req_analysis_port.write(item);
         // After picking up a request, wait until a response is sent before
         // detecting another request, as this is not a pipelined protocol.
         `DV_SPINWAIT_EXIT(while (!cfg.vif.mon_cb.cmd_rsp.csrng_rsp_ack) @(cfg.vif.mon_cb);,

hw/dv/sv/csrng_agent/csrng_sequencer.sv
@@ -11,17 +11,6 @@ class csrng_sequencer extends dv_base_sequencer #(
   push_pull_sequencer#(.HostDataWidth(csrng_pkg::CSRNG_CMD_WIDTH))          m_req_push_sequencer;
   push_pull_sequencer#(.HostDataWidth(csrng_pkg::FIPS_GENBITS_BUS_WIDTH))   m_genbits_push_sequencer;
 
-  // Analysis port through which device monitors can send transactions
-  // to the sequencer.
-  uvm_tlm_analysis_fifo#(csrng_item)   cmd_req_fifo;
-
   `uvm_component_new
-
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    if (cfg.if_mode == dv_utils_pkg::Device) begin
-      cmd_req_fifo = new("cmd_req_fifo", this);
-    end
-  endfunction
 
 endclass

hw/dv/sv/csrng_agent/seq_lib/csrng_device_seq.sv
@@ -18,7 +18,7 @@ class csrng_device_seq extends csrng_base_seq;
     fork
       forever begin
         csrng_item   req;
-        p_sequencer.cmd_req_fifo.get(req);
+        p_sequencer.req_analysis_fifo.get(req);
         req_q.push_back(req);
       end
 

hw/dv/sv/dv_lib/dv_base_agent.sv
@@ -43,16 +43,24 @@ class dv_base_agent #(type CFG_T            = dv_base_agent_cfg,
       sequencer = SEQUENCER_T::type_id::create("sequencer", this);
       sequencer.cfg = cfg;
 
-      if (cfg.if_mode == Host)  driver = HOST_DRIVER_T::type_id::create("driver", this);
-      else                      driver = DEVICE_DRIVER_T::type_id::create("driver", this);
-      driver.cfg = cfg;
+      if (cfg.has_driver) begin
+        if (cfg.if_mode == Host)  driver = HOST_DRIVER_T::type_id::create("driver", this);
+        else                      driver = DEVICE_DRIVER_T::type_id::create("driver", this);
+        driver.cfg = cfg;
+      end
     end
   endfunction
 
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    if (cfg.is_active) begin
+    if (cfg.is_active && cfg.has_driver) begin
       driver.seq_item_port.connect(sequencer.seq_item_export);
+    end
+    if (cfg.has_req_fifo) begin
+      monitor.req_analysis_port.connect(sequencer.req_analysis_fifo.analysis_export);
+    end
+    if (cfg.has_rsp_fifo) begin
+      monitor.rsp_analysis_port.connect(sequencer.rsp_analysis_fifo.analysis_export);
     end
   endfunction
 

hw/dv/sv/dv_lib/dv_base_agent_cfg.sv
@@ -5,17 +5,27 @@
 class dv_base_agent_cfg extends uvm_object;
 
   // agent cfg knobs
-  bit         is_active = 1'b1;   // active driver or passive monitor
+  bit         is_active = 1'b1;   // active driver/sequencer or passive monitor
   bit         en_cov    = 1'b1;   // enable coverage
   if_mode_e   if_mode;            // interface mode - Host or Device
+
+  // indicate to create and connet driver to sequencer or not
+  // if this is a high-level agent, we may just call lower-level agent to send item in seq, then
+  // driver isn't needed
+  bit         has_driver = 1'b1;
+  // indicate if these fifo and ports exist or not
+  bit         has_req_fifo = 1'b0;
+  bit         has_rsp_fifo = 1'b0;
 
   // use for phase_ready_to_end to add additional delay after ok_to_end is set
   int ok_to_end_delay_ns = 1000;
 
   `uvm_object_utils_begin(dv_base_agent_cfg)
-    `uvm_field_int (is_active,          UVM_DEFAULT)
-    `uvm_field_int (en_cov,             UVM_DEFAULT)
-    `uvm_field_enum(if_mode_e, if_mode, UVM_DEFAULT)
+    `uvm_field_int (is_active,            UVM_DEFAULT)
+    `uvm_field_int (en_cov,               UVM_DEFAULT)
+    `uvm_field_enum(if_mode_e, if_mode,   UVM_DEFAULT)
+    `uvm_field_int (has_req_fifo,         UVM_DEFAULT)
+    `uvm_field_int (has_rsp_fifo,         UVM_DEFAULT)
   `uvm_object_utils_end
 
   `uvm_object_new

commit 38eb93e99a1ebbf5cb754917560913da6f7969a4

[dv] Allow monitor items to have different types from sequence items
This was broken by 5954b13. This isn't the cleanest fix, because the
monitor class is still parameterised by types that it has no business
knowing about.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/dv/sv/dv_lib/dv_base_monitor.sv
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 class dv_base_monitor #(type ITEM_T = uvm_sequence_item,
+                        type REQ_ITEM_T = ITEM_T,
+                        type RSP_ITEM_T = ITEM_T,
                         type CFG_T  = dv_base_agent_cfg,
                         type COV_T  = dv_base_agent_cov) extends uvm_monitor;
   `uvm_component_param_utils(dv_base_monitor #(ITEM_T, CFG_T, COV_T))
@@ -21,9 +23,9 @@ class dv_base_monitor #(type ITEM_T = uvm_sequence_item,
   uvm_analysis_port #(ITEM_T) analysis_port;
 
   // item will be sent to this port for seq when req phase is done (last is set)
-  uvm_analysis_port #(ITEM_T) req_analysis_port;
+  uvm_analysis_port #(REQ_ITEM_T) req_analysis_port;
   // item will be sent to this port for seq when rsp phase is done (rsp_done is set)
-  uvm_analysis_port #(ITEM_T) rsp_analysis_port;
+  uvm_analysis_port #(RSP_ITEM_T) rsp_analysis_port;
 
   `uvm_component_new
 

hw/dv/sv/dv_lib/dv_base_sequencer.sv
@@ -15,8 +15,8 @@ class dv_base_sequencer #(type ITEM_T     = uvm_sequence_item,
   // monitor and sequences. These fifos are optional
   // When device is re-active, it gets items from req_analysis_fifo and send rsp to driver
   // When this is a high-level agent, monitors put items to these 2 fifos for high-level seq
-  uvm_tlm_analysis_fifo #(ITEM_T) req_analysis_fifo;
-  uvm_tlm_analysis_fifo #(ITEM_T) rsp_analysis_fifo;
+  uvm_tlm_analysis_fifo #(ITEM_T)     req_analysis_fifo;
+  uvm_tlm_analysis_fifo #(RSP_ITEM_T) rsp_analysis_fifo;
 
   CFG_T cfg;
 