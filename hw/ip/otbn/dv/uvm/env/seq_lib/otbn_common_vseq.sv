emmau678/opentitan

commit fd4686d8df17ba70b75168810f1c4eb24280a0e5

[otbn,dv] Sort out timing for done/status signals in ISS
Now that otbn_core_model exposes a STATUS register, we can use it to
derive the "done" signal. Using the status register directly, rather
than reconstructing it from the done signal, models the OTBN block
more closely and actually simplifies a load of tracking DV
code (mainly because the two things that we're comparing Just Match,
so we don't need to convert between the two views of the world).

Unfortunately, we can't get rid of the "done" signal entirely, because
otbn.sv can choose to instantiate the model instead of the RTL to
represent the core. The timing isn't *quite* right here, because the
core flops STATUS but not its done_o output. But this shouldn't matter
for the chip-level sims where we use the model like this.

We *do* check that the RTL's done signal is as predicted in the UVM
and verilator testbenches.

This patch also sorts out the timing of the start of execution in the
UVM testbench: the ISS was probing the wrong start signal, so started
a cycle late. This didn't matter too much, because it and the design
were both waiting on the EDN, which took more than one cycle.

Finally, we fix the assertion that checks the model and RTL have
matching STATUS. This was broken before (using "rst_n", rather than
"!rst_n"), which is why we didn't notice the off-by-one at the start.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/uvm/tb.sv
@@ -158,7 +158,7 @@ module tb;
   // to grab the decoded signal from TL transactions on the cycle it happens. We have an explicit
   // check in the scoreboard to ensure that this gets asserted at the time we expect (to spot any
   // decoding errors).
-  assign model_if.start = dut.start_q;
+  assign model_if.start = dut.start_d;
 
   // Valid signals below are set when DUT finishes processing incoming 32b packages and constructs
   // 256b EDN data. Model checks if the processing of the packages are done in maximum of 5 cycles
@@ -183,7 +183,6 @@ module tb;
     .rst_edn_ni   (edn_rst_n),
 
     .start_i      (model_if.start),
-    .done_o       (model_if.done),
 
     .err_bits_o   (),
 
@@ -194,6 +193,8 @@ module tb;
 
     .status_o     (model_if.status),
     .insn_cnt_o   (model_insn_cnt),
+
+    .done_r_o     (),
 
     .err_o        (model_if.err)
   );
@@ -224,12 +225,11 @@ module tb;
   // These are the sort of checks that are easier to state at the design level than in terms of UVM
   // transactions.
 
-  // Check that the idle output from the DUT is the inverse of the model's "done" signal.
-  // Separately, the code in otbn_idle_checker.sv checks that the idle output from the DUT is also
-  // the inverse of the "done" signal that we expect. Combining the two tells us that the RTL and
-  // model agree about whether they are running or not.
-  `ASSERT(MatchingDone_A, idle == !model_if.done, clk, rst_n)
-
+  // Check that the status output from the model exactly matches the register from the dut. In
+  // theory, we could see mismatches by probing the STATUS register over the TL bus, but we have to
+  // be lucky with exactly when the reads happen if we want to see off-by-one cycle errors. This
+  // assertion gives a continuous check.
+  `ASSERT(MatchingStatus_A, dut.hw2reg.status.d == model_if.status, clk, !rst_n)
 
   initial begin
     mem_bkdr_util imem_util, dmem_util;

hw/ip/otbn/dv/model/otbn_core_model.sv
@@ -35,9 +35,8 @@ module otbn_core_model
   input  logic                     rst_edn_ni,
 
   input  logic                     start_i, // start the operation
-  output bit                       done_o,  // operation done
 
-  output err_bits_t                err_bits_o, // valid when done_o is asserted
+  output err_bits_t                err_bits_o, // updated when STATUS switches to idle
 
   input  edn_pkg::edn_rsp_t        edn_rnd_i, // EDN response interface
   input  logic                     edn_rnd_cdc_done_i, // RND from EDN is valid (DUT perspective)
@@ -45,6 +44,8 @@ module otbn_core_model
 
   output bit [7:0]       status_o,   // STATUS register
   output bit [31:0]      insn_cnt_o, // INSN_CNT register
+
+  output bit             done_r_o,
 
   output bit             err_o // something went wrong
 );
@@ -101,9 +102,7 @@ module otbn_core_model
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      if (model_state != 0) begin
-        otbn_model_reset(model_handle);
-      end
+      otbn_model_reset(model_handle);
       model_state <= 0;
       status_q <= 0;
       insn_cnt_q <= 0;
@@ -140,17 +139,6 @@ module otbn_core_model
   assign status_o = status_q;
   assign insn_cnt_o = insn_cnt_q;
 
-  // Track negedges of running_q and expose that as a "done" output.
-  bit running_r = 1'b0;
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      running_r <= 1'b0;
-    end else begin
-      running_r <= running;
-    end
-  end
-  assign done_o = running_r & ~running;
-
   // If DesignScope is not empty, we have a design to check. Bind a copy of otbn_rf_snooper_if into
   // each register file. The otbn_model_check() function will use these to extract memory contents.
   if (DesignScope != "") begin: g_check_design
@@ -181,6 +169,20 @@ module otbn_core_model
   end
 
   assign err_o = failed_step | failed_cmp;
+
+  // Derive a "done" signal. This should trigger for a single cycle when OTBN finishes its work.
+  // It's analogous to the done_o signal on otbn_core, but this signal is delayed by a single cycle
+  // (hence its name is done_r_o).
+  bit otbn_model_busy, otbn_model_busy_r;
+  assign otbn_model_busy = (status_q != StatusIdle) && (status_q != StatusLocked);
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      otbn_model_busy_r <= 1'b0;
+    end else begin
+      otbn_model_busy_r <= otbn_model_busy;
+    end
+  end
+  assign done_r_o = otbn_model_busy_r & ~otbn_model_busy;
 
   // Make stop_pc available over DPI. This is handy for Verilator simulations (where the top-level
   // is in C++).

hw/ip/otbn/dv/otbnsim/sim/sim.py
@@ -120,8 +120,9 @@ class OTBNSim:
             # Zero INSN_CNT the cycle after we are told to start (and every
             # cycle after that until we start executing instructions, but that
             # doesn't really matter)
+            changes = self._on_stall(verbose, fetch_next=False)
             self.state.ext_regs.write('INSN_CNT', 0, True)
-            return (None, self._on_stall(verbose, fetch_next=False))
+            return (None, changes)
 
         insn = self._next_insn
         if insn is None:

hw/ip/otbn/dv/otbnsim/sim/state.py
@@ -157,8 +157,9 @@ class OTBNState:
         if self.rnd_cdc_pending:
             self.rnd_cdc_counter += 1
 
+        self.ext_regs.commit()
+
         if self._urnd_stall:
-            self.ext_regs.commit()
             if self._urnd_reseed_complete:
                 self._urnd_stall = False
                 self.non_insn_stall = False

hw/ip/otbn/dv/uvm/env/otbn_scoreboard.sv
@@ -42,11 +42,20 @@ class otbn_scoreboard extends cip_base_scoreboard #(
   // array, mapping the transaction source ID (a_source in the TL transaction) to an expected value.
   otbn_exp_read_data_t exp_read_values [tl_source_t];
 
-  bit saw_start_tl_trans = 1'b0;
-  bit waiting_for_model = 1'b0;
+  // A flag that tracks the fact that we've seen a TL write to the CMD register that we expect to
+  // start OTBN. We track this because we derive the "start" signal in the model from an internal
+  // DUT signal, so need to make sure it stays in sync with the TL side.
+  //
+  // If false, there are no transactions pending. This gets set in process_tl_addr when we see a
+  // write to CMD. This runs on a posedge of the clock. We expect to see the model start on the
+  // following negedge. When we process a change to model status that shows things starting (in
+  // process_model_fifo), we check that the flag is set and then clear it. When setting the flag, we
+  // queue a check to run on the following posedge of the clock to make sure the flag isn't still
+  // set.
+  bit pending_start_tl_trans = 1'b0;
 
   // The mirrored STATUS register from the ISS.
-  bit [7:0] model_status = 1'b0;
+  bit [7:0] model_status = 8'd0;
 
   // The "locked" field is used to track whether OTBN is "locked". For most operational state
   // tracking, we go through the ISS, but OTBN can become locked without actually starting an
@@ -72,7 +81,6 @@ class otbn_scoreboard extends cip_base_scoreboard #(
     fork
       process_model_fifo();
       process_trace_fifo();
-      check_start();
     join_none
   endtask
 
@@ -83,15 +91,8 @@ class otbn_scoreboard extends cip_base_scoreboard #(
     // transaction, the reset will have caused them to be forgotten.
     exp_read_values.delete();
 
-    // Clear waiting_for_model. This handles a corner case where a TL transaction comes in to start
-    // OTBN just before a reset and the reset hits after the negedge of the clock (so we have set
-    // waiting_for_model) but before the following clock cycle (which would have generated the
-    // OtbnModelStart transaction).
-    waiting_for_model = 1'b0;
-
-    // Clear saw_start_tl_trans. Any "start" TL transaction will have been discarded by the reset,
-    // so we shouldn't still be tracking it.
-    saw_start_tl_trans = 1'b0;
+    model_status = otbn_pkg::StatusIdle;
+    pending_start_tl_trans = 1'b0;
 
     // Clear the locked bit (this is modelling RTL state that should be cleared on reset)
     locked = 1'b0;
@@ -128,17 +129,22 @@ class otbn_scoreboard extends cip_base_scoreboard #(
       end
 
       case (csr.get_name())
-        // Spot writes to the "cmd" register, which tell us to start
-        // These start the processor. We don't pass those to the model through UVM, because it's
-        // difficult to get the timing right (you only recognise the transaction on the clock edge
-        // after you needed to set the signal!), so the testbench actually grabs the model's start
-        // signal from the DUT internals. Of course, we need to check this is true exactly when we
-        // expect it to be. Here, we set a flag to say that we expect the "start" signal to be high.
-        // See the check_start() task, which checks it's true at the right time.
+        // Spot writes to the "cmd" register that tell us to start
         "cmd": begin
-          // We start the execution when we see a write of the EXECUTE command.
+          // We start the execution when we see a write of the EXECUTE command. See the comment
+          // above pending_start_tl_trans to see how this tracking works.
           if (item.a_data == otbn_pkg::CmdExecute) begin
-            saw_start_tl_trans = 1'b1;
+            // Set a flag: we're expecting the model to start on the next negedge. Also, spawn off a
+            // checking thread that will make sure it has been cleared again by the next posedge.
+            // Note that the reset() method is only called in the DV base class on the following
+            // posedge of rst_n, so we have to check whether we're still in reset here.
+            pending_start_tl_trans = 1'b1;
+            fork begin
+              @(cfg.clk_rst_vif.cb);
+              `DV_CHECK_FATAL(!cfg.clk_rst_vif.rst_n || !pending_start_tl_trans,
+                              "Model ignored a write of EXECUTE to the CMD register.")
+            end
+            join_none;
           end
         end
 
@@ -257,14 +263,18 @@ class otbn_scoreboard extends cip_base_scoreboard #(
       `uvm_info(`gfn, $sformatf("received model transaction:\n%0s", item.sprint()), UVM_HIGH)
 
       case (item.item_type)
-        OtbnModelStart: begin
-          // We should only see the model start if it was started by a TL transaction on the
-          // previous cycle.
-          `DV_CHECK_FATAL(waiting_for_model, "model started unbidden!")
-          waiting_for_model = 1'b0;
-        end
-
         OtbnModelStatus: begin
+          // Has the status changed from idle to busy? If so, we should have seen a write to the
+          // command register on the previous posedge. See comment above pending_start_tl_trans for
+          // the details.
+          if (model_status == otbn_pkg::StatusIdle &&
+              item.status inside {otbn_pkg::StatusBusyExecute,
+                                  otbn_pkg::StatusBusySecWipeDmem,
+                                  otbn_pkg::StatusBusySecWipeImem}) begin
+            `DV_CHECK_FATAL(pending_start_tl_trans,
+                            "Saw start transaction without corresponding write to CMD")
+            pending_start_tl_trans = 1'b0;
+          end
           model_status = item.status;
         end
 
@@ -297,36 +307,6 @@ class otbn_scoreboard extends cip_base_scoreboard #(
 
       rtl_trace_queue.push_back(item);
       pop_trace_queues();
-    end
-  endtask
-
-  // We track TL writes that should start the processor and the model transaction that says it has
-  // started. There should be a single cycle delay between the two: on the negedge of each clock,
-  // convert a "there was a TL write" event (saw_start_tl_trans) to "we expect to see the model
-  // start" (waiting_for_model).
-  //
-  // If we get to a negedge where waiting_for_model is true, that means that the model didn't start
-  // when we expected it to.
-  task check_start();
-    forever begin
-      @(cfg.clk_rst_vif.cbn);
-
-      if (!cfg.clk_rst_vif.rst_n) begin
-        // We're in reset. Wait until we start again.
-        @(posedge cfg.clk_rst_vif.rst_n);
-      end else begin
-        // We're not in reset. Check that waiting_for_model is false. If not, we've had a cycle
-        // since the model should have started.
-        `DV_CHECK(!waiting_for_model, "model didn't start when we expected it to")
-
-        // If we've just seen a write to the CMD register that should start OTBN, set the
-        // waiting_for_model flag. This gives a single cycle delay (on the next cycle, we'll check
-        // it worked properly using the DV_CHECK above).
-        if (saw_start_tl_trans) begin
-          waiting_for_model = 1'b1;
-          saw_start_tl_trans = 1'b0;
-        end
-      end
     end
   endtask
 

hw/ip/otbn/dv/uvm/otbn_model_agent/otbn_model_agent_pkg.sv
@@ -12,7 +12,6 @@ package otbn_model_agent_pkg;
     otbn_trace_checker_pop_iss_insn(output bit [31:0] insn_addr, output string mnemonic);
 
   typedef enum {
-    OtbnModelStart,
     OtbnModelStatus,
     OtbnModelInsn
   } otbn_model_item_type_e;

hw/ip/otbn/dv/uvm/otbn_model_agent/otbn_model_if.sv
@@ -16,7 +16,6 @@ interface otbn_model_if #(
   logic                     start;        // Start the operation
 
   // Outputs from DUT
-  bit                       done;         // Operation done
   bit                       err;          // Something went wrong
   bit [31:0]                stop_pc;      // PC at end of operation
 

hw/ip/otbn/dv/uvm/otbn_model_agent/otbn_model_monitor.sv
@@ -21,30 +21,10 @@ class otbn_model_monitor extends dv_base_monitor #(
 
   protected task collect_trans(uvm_phase phase);
     fork
-      collect_start();
       collect_status();
       // TODO: Only run when coverage is enabled.
       collect_insns();
     join
-  endtask
-
-  protected task collect_start();
-    otbn_model_item trans;
-
-    forever begin
-      // Collect transactions on each clock edge when reset is high.
-      @(posedge cfg.vif.clk_i);
-      if (cfg.vif.rst_ni === 1'b1) begin
-        if (cfg.vif.start) begin
-          trans = otbn_model_item::type_id::create("trans");
-          trans.item_type = OtbnModelStart;
-          trans.status    = 0;
-          trans.err       = 0;
-          trans.mnemonic  = "";
-          analysis_port.write(trans);
-        end
-      end
-    end
   endtask
 
   protected task collect_status();

commit 7a21103ccbce92fbd5e4e0025773bfe28f102c21

[otbn,dv] Disable MatchingStatus_A assertion in integrity error vseq
This fixes various nightly test failures that started with commit
fd4686d (which fixed the assertion, which was previously broken and
never fired!)

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/uvm/env/seq_lib/otbn_common_vseq.sv
@@ -45,4 +45,14 @@ class otbn_common_vseq extends otbn_base_vseq;
     end
   endtask
 
+  // Overridden from cip_base_vseq. Disable the MatchingStatus_A assertion from the testbench for
+  // this sequence. This assertion checks that the model's STATUS register matches the DUT. Since we
+  // don't actually start the processor or model (or, indeed, tell the model about the error), this
+  // assertion will be false.
+  task run_tl_intg_err_vseq(int num_times = 1);
+    `DV_ASSERT_CTRL_REQ("otbn_status_assert_en", 1'b0)
+    super.run_tl_intg_err_vseq(num_times);
+    `DV_ASSERT_CTRL_REQ("otbn_status_assert_en", 1'b1)
+  endtask
+
 endclass

hw/ip/otbn/dv/uvm/tb.sv
@@ -30,6 +30,10 @@ module tb;
   pins_if #(NUM_MAX_INTERRUPTS) intr_if    (interrupts);
   assign interrupts[0] = {intr_done};
 
+  // A hook to allow sequences to enable or disable the MatchingStatus_A assertion below. This is
+  // needed for sequences that trigger alerts (locking OTBN) without telling the model.
+  `DV_ASSERT_CTRL("otbn_status_assert_en", tb.MatchingStatus_A)
+
   otbn_model_if #(
     .ImemSizeByte (otbn_reg_pkg::OTBN_IMEM_SIZE)
   ) model_if (