emmau678/opentitan

commit 60208fd7377259748601b8326d931160576a6f81

[otbn,dv] Tweak how we randomise flip_bits in otbn_common_vseq
Xcelium doesn't allow randomising an entry in an arry ("The argument
to class or scope randomize must be a simple identifier of integral
type.").

It turns out that we can shuffle things around a bit to simplify how
everything works, so I've done that too.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/uvm/env/seq_lib/otbn_common_vseq.sv
@@ -67,36 +67,42 @@ class otbn_common_vseq extends otbn_base_vseq;
 
   virtual function void inject_intg_fault_in_passthru_mem(dv_base_mem mem,
                                                           bit [bus_params_pkg::BUS_AW-1:0] addr);
-    bit [38:0]       rdata_imem;
-    bit [311:0]      rdata_dmem;
-    bit [38:0]       flip_bits_imem;
-    bit [311:0]      flip_bits_dmem;
-    bit [38:0]       flip_bits_dmem_arr [8];
+    logic [127:0] key;
+    logic [63:0]  nonce;
+    bit [38:0]    flip_bits;
 
-    logic [127:0]    key;
-    logic [63:0]     nonce;
+    `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(
+        flip_bits,
+        $countones(flip_bits) inside {[1:cip_base_pkg::MAX_TL_ECC_ERRORS]};)
 
     if(mem.get_name() == "imem") begin
+      bit [38:0] rdata;
+
       key   = cfg.get_imem_key();
       nonce = cfg.get_imem_nonce();
-      `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(flip_bits_imem,
-          $countones(flip_bits_imem) inside {[1:cip_base_pkg::MAX_TL_ECC_ERRORS]};)
-      rdata_imem = cfg.read_imem_word(addr / 4, key, nonce);
-      `uvm_info(`gfn, $sformatf("Backdoor change mem (addr 0x%0h) value 0x%0h by flipping bits %0h",
-                              addr, rdata_imem, flip_bits_imem), UVM_LOW)
-      cfg.write_imem_word(addr / 4, rdata_imem, key, nonce, flip_bits_imem);
+      rdata = cfg.read_imem_word(addr / 4, key, nonce);
+      `uvm_info(`gfn,
+                $sformatf("Backdoor change IMEM (addr 0x%0h) value 0x%0h by flipping bits %0h",
+                          addr, rdata, flip_bits),
+                UVM_LOW)
+      cfg.write_imem_word(addr / 4, rdata, key, nonce, flip_bits);
     end
     else begin
+      bit [311:0] rdata;
+      bit [311:0] rep_flip_bits;
+
+      rep_flip_bits = {8{flip_bits}};
+
       key   = cfg.get_dmem_key();
       nonce = cfg.get_dmem_nonce();
-      `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(flip_bits_dmem_arr[addr[7:0] / 4],
-          $countones(flip_bits_dmem_arr[addr[7:0] / 4])
-          inside {[1:cip_base_pkg::MAX_TL_ECC_ERRORS]};)
-      flip_bits_dmem = {<<{flip_bits_dmem_arr}};
-      rdata_dmem = cfg.read_dmem_word(addr / 32, key, nonce);
-      `uvm_info(`gfn, $sformatf("Backdoor change mem (addr 0x%0h) value 0x%0h by flipping bits %0h",
-                                addr, rdata_dmem, flip_bits_dmem), UVM_LOW)
-      cfg.write_dmem_word(addr / 32, rdata_dmem, key, nonce, flip_bits_dmem);
+      rdata = cfg.read_dmem_word(addr / 32, key, nonce);
+
+      `uvm_info(`gfn,
+                $sformatf("Backdoor change DMEM (addr 0x%0h) value 0x%0h by flipping bits %0h",
+                          addr, rdata, rep_flip_bits),
+                UVM_LOW)
+
+      cfg.write_dmem_word(addr / 32, rdata, key, nonce, flip_bits);
     end
 
   endfunction

commit 8eed3905276a9b5fb16873be3c14c33025cede4c

[otbn,dv] Flip bits in all DMEM words in OTBN passthru sequence
This worked just fine until I broke it in 60208fd737 by forgetting to
update a variable name. Oops!

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/uvm/env/seq_lib/otbn_common_vseq.sv
@@ -102,7 +102,7 @@ class otbn_common_vseq extends otbn_base_vseq;
                           addr, rdata, rep_flip_bits),
                 UVM_LOW)
 
-      cfg.write_dmem_word(addr / (4 * BaseWordsPerWLEN), rdata, key, nonce, flip_bits);
+      cfg.write_dmem_word(addr / (4 * BaseWordsPerWLEN), rdata, key, nonce, rep_flip_bits);
     end
 
   endfunction