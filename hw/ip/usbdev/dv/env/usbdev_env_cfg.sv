emmau678/opentitan

commit 82b55d472ae825d0a8975136b73e926909f673dd

[usbdev,dv] Fix speed of AON clock
Before this patch, the AON clock was faster than the 48MHz USB clock.
This is backwards: it's supposed to be much slower (and low power!).
Switch the two around.

The real speed of the AON clock is going to be even slower than is
configured here, but having the two be reasonably similar should give
the same test efficacy and make the tests quicker to run (and debug!).

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/usbdev/dv/env/usbdev_env_cfg.sv
@@ -15,13 +15,13 @@ class usbdev_env_cfg extends cip_base_env_cfg #(.RAL_T(usbdev_reg_block));
     usb_clk_freq_mhz == 48;
   }
 
-  // Constrain the AON clock to be faster than the USB clock. Make it between 2 and 3 times the
-  // speed. No serious requirements, but making them more noticeably different makes it easier to
-  // look at wave dumps.
+  // Constrain the AON clock to be slower than the USB clock. Make it between 1/2 and 1/3 of the
+  // speed. (This isn't as big a ratio as we'll have on the chip, but the tests are a bit more
+  // efficient when the clocks are similar speeds)
   rand uint aon_clk_freq_mhz;
   constraint aon_clk_freq_mhz_c {
-    aon_clk_freq_mhz >= usb_clk_freq_mhz * 2 &&
-    aon_clk_freq_mhz <= usb_clk_freq_mhz * 3;
+    aon_clk_freq_mhz * 3 >= usb_clk_freq_mhz &&
+    aon_clk_freq_mhz * 2 <= usb_clk_freq_mhz;
   }
 
   // ext component cfgs

commit 8ff114bf3907a4f9738c2409ae38047ee7a30ba5

[usbdev,dv] Tighten constraint on AON clock frequency
This fixes a rather confusing bug that I introduced last July with
82b55d472ae. My change there was reasonable, except that we were now
only constraining aon_clk_freq_mhz after it was multiplied by 2 or 3.

Since these are 32-bit unsigned integers, weird things happened if the
initial guess for the value was "2^31 + N". Then the constraints were:

 1.) aon_clk_freq_mhz * 3 >= usb_clk_freq_mhz

     This would check that 3 * (2^31 + N) was large enough. But that
     wraps to 2^31 + 3*N, which is pretty big.

 2.) aon_clk_freq_mhz * 2 <= usb_clk_freq_mhz;

     This would check that 2 * (2^31 + N) was small enough. But that
     wraps to 2*N which can be nice and small.

Oops!

To avoid this problem, divide through in both inequalities so that the
only problem is loss of information from underflow. Not an issue in
this situation (my original code used multiplication because I've got
a signal processing background where you *never* want to do division!)

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/usbdev/dv/env/usbdev_env_cfg.sv
@@ -20,8 +20,8 @@ class usbdev_env_cfg extends cip_base_env_cfg #(.RAL_T(usbdev_reg_block));
   // efficient when the clocks are similar speeds)
   rand uint aon_clk_freq_mhz;
   constraint aon_clk_freq_mhz_c {
-    aon_clk_freq_mhz * 3 >= usb_clk_freq_mhz &&
-    aon_clk_freq_mhz * 2 <= usb_clk_freq_mhz;
+    aon_clk_freq_mhz >= usb_clk_freq_mhz / 3 &&
+    aon_clk_freq_mhz <= usb_clk_freq_mhz / 2;
   }
 
   // ext component cfgs