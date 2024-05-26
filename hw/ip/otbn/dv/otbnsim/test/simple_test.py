8008b94bb3356c20cbb877accc1ad9421879c7da
emmau678 <eu233@Emma-laptop>
2024-05-26 17:29:50 +0100

    Extend the test framework in simple_test.py
    Adapt the test functionality to accept the directory from which to gather test files as an arg. Enhance debugging capabilities within OTBNSim environment by integrating the otbn_sim_test function from opentitan/hw/ip/otbn/util/otbn_sim_test.py, which reports error names and line number at which the fault occurred.

--- hw/ip/otbn/dv/otbnsim/test/simple_test.py
+++ hw/ip/otbn/dv/otbnsim/test/simple_test.py
@@ -1,4 +1,4 @@
-# Copyright lowRISC contributors (OpenTitan project).
+# Copyright lowRISC contributors.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
@@ -16,17 +16,34 @@ import subprocess
 from typing import Any, List, Tuple
 
 from testutil import asm_and_link_one_file, SIM_DIR
+
+import argparse
+import subprocess
+import sys
+from enum import IntEnum
+from typing import List
+
+from shared.check import CheckResult
 from shared.reg_dump import parse_reg_dump
 
+# Names of special registers
+ERR_BITS = 'ERR_BITS'
+INSN_CNT = 'INSN_CNT'
+STOP_PC = 'STOP_PC'
 
-def find_simple_tests() -> List[Tuple[str, str]]:
-    '''Find all tests below ./simple (relative to this file)
+from pathlib import Path
+
+output_dir = Path('/tmp/pytest-of-eu233/pytest-7/test_count_test1_s_0')
+output_dir.mkdir(parents=True, exist_ok=True)
+
+def find_tests(dir) -> List[Tuple[str, str]]:
+    '''Find all tests below ./{dirname} (relative to this file)
 
     Returns (asm, expected) pairs, with the paths to the assembly file and
     expected values.
 
     '''
-    root = os.path.join(os.path.dirname(__file__), 'simple')
+    root = os.path.join(os.path.dirname(__file__), dir)
     ret = []
     for subdir, _, files in os.walk(root):
         # We're interested in pairs foo.s / foo.exp, which contain the assembly
@@ -71,11 +88,16 @@ def find_simple_tests() -> List[Tuple[str, str]]:
                                    .format(dirname, exp_file, basename))
 
         assert len(exp_files) == len(asm_files)
-
     return ret
 
 
 def test_count(tmpdir: py.path.local,
+               asm_file: str,
+               expected_file: str) -> None:
+        assert helper_test_count(tmpdir=tmpdir, asm_file=asm_file, expected_file=expected_file)
+
+
+def helper_test_count(tmpdir: py.path.local,
                asm_file: str,
                expected_file: str) -> None:
     # Start by assembling and linking the input file
@@ -83,26 +105,131 @@ def test_count(tmpdir: py.path.local,
 
     # Run the simulation. We can just pass a list of commands to stdin, and
     # don't need to do anything clever to track what's going on.
-    cmd = [os.path.join(SIM_DIR, 'standalone.py'),
-           '--dump-regs', '-', elf_file]
-    sim_proc = subprocess.run(cmd, check=True,
-                              stdout=subprocess.PIPE, universal_newlines=True)
+    sim_file = os.path.join(SIM_DIR, 'standalone.py')
 
-    regs_seen = parse_reg_dump(sim_proc.stdout)
-    with open(expected_file) as exp_file:
-        regs_expected = parse_reg_dump(exp_file.read())
-
-    # If a register does not appear in the expected registers, then its value
-    # is not constrained; set the expected value to match the actual value.
-    for reg in regs_seen:
-        if reg not in regs_expected:
-            regs_expected[reg] = regs_seen[reg]
-
-    assert regs_expected == regs_seen
+    args_list = [sim_file, expected_file, elf_file, "--verbose"]
+    return not otbn_sim_test(args_list)
 
 
 def pytest_generate_tests(metafunc: Any) -> None:
     if metafunc.function is test_count:
-        tests = find_simple_tests()
+        tests = find_tests("simple")
         test_ids = [os.path.basename(e[0]) for e in tests]
         metafunc.parametrize("asm_file,expected_file", tests, ids=test_ids)
+
+
+######## Below code is copied from hw/ip/otbn/util/otbn_sim_test.py to enhance
+# debugging capabilities. Identifies error type and reports line number at
+# which the fault occurred. ########
+    
+# copied from hw/ip/otbn/dv/otbnsim/sim/constants.py
+class ErrBits(IntEnum):
+    '''A copy of the list of bits in the ERR_BITS register.'''
+    BAD_DATA_ADDR = 1 << 0
+    BAD_INSN_ADDR = 1 << 1
+    CALL_STACK = 1 << 2
+    ILLEGAL_INSN = 1 << 3
+    LOOP = 1 << 4
+    KEY_INVALID = 1 << 5
+    RND_REP_CHK_FAIL = 1 << 6
+    RND_FIPS_CHK_FAIL = 1 << 7
+    IMEM_INTG_VIOLATION = 1 << 16
+    DMEM_INTG_VIOLATION = 1 << 17
+    REG_INTG_VIOLATION = 1 << 18
+    BUS_INTG_VIOLATION = 1 << 19
+    BAD_INTERNAL_STATE = 1 << 20
+    ILLEGAL_BUS_ACCESS = 1 << 21
+    LIFECYCLE_ESCALATION = 1 << 22
+    FATAL_SOFTWARE = 1 << 23
+
+
+def get_err_names(err: int) -> List[str]:
+    '''Get the names of all error bits that are set.'''
+    out = []
+    for err_bit in ErrBits:
+        if err & err_bit != 0:
+            out.append(err_bit.name)
+    return out
+
+
+def otbn_sim_test(args_list):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('simulator',
+                        help='Path to the standalone OTBN simulator.')
+    parser.add_argument('expected',
+                        metavar='FILE',
+                        type=argparse.FileType('r'),
+                        help=(f'File containing expected register values. '
+                              f'Registers that are not listed are allowed to '
+                              f'have any value, except for {ERR_BITS}. If '
+                              f'{ERR_BITS} is not listed, the test will assume '
+                              f'there are no errors expected (i.e. {ERR_BITS}'
+                              f'= 0).'))
+    parser.add_argument('elf',
+                        help='Path to the .elf file for the OTBN program.')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args(args_list)
+
+    # Parse expected values.
+    result = CheckResult()
+    expected_regs = parse_reg_dump(args.expected.read())
+
+    # Run the simulation and produce a register dump.
+    cmd = [args.simulator, '--dump-regs', '-', '--dump-dmem', 'memdump.txt', args.elf]
+    sim_proc = subprocess.run(cmd, check=True,
+                              stdout=subprocess.PIPE, universal_newlines=True)
+    actual_regs = parse_reg_dump(sim_proc.stdout)
+
+    # Special handling for the ERR_BITS register.
+    expected_err = expected_regs.get(ERR_BITS, 0)
+    actual_err = actual_regs[ERR_BITS]
+    insn_cnt = actual_regs[INSN_CNT]
+    stop_pc = actual_regs[STOP_PC]
+    if expected_err == 0 and actual_err != 0:
+        # Test is expected to have no errors, but an error occurred. In this
+        # case, give a special error message and exit rather than print all the
+        # mismatched registers.
+        if actual_err != 0:
+            err_names = ', '.join(get_err_names(actual_err))
+            result.err(f'OTBN encountered an unexpected error: {err_names}.\n'
+                       f'  {ERR_BITS}\t= {actual_err:#010x}\n'
+                       f'  {INSN_CNT}\t= {insn_cnt:#010x}\n'
+                       f'  {STOP_PC}\t= {stop_pc:#010x}')
+    else:
+        for reg, expected_value in expected_regs.items():
+            actual_value = actual_regs.get(reg, None)
+            if actual_value != expected_value:
+                # Handle signed expected_value by constructing its 2's complement if it's negative
+                if expected_value < 0:
+                    mask = (1 << 256) - 1
+                    expected_value_unsigned = ((abs(expected_value) ^ mask) + 1) & mask
+                else:
+                    expected_value_unsigned = expected_value
+
+                if actual_value < 0:
+                    mask = (1 << 256) - 1
+                    actual_value_unsigned = ((abs(actual_value) ^ mask) + 1) & mask
+                else:
+                    actual_value_unsigned = actual_value
+            
+                if reg.startswith('w'):
+                    if actual_value_unsigned != expected_value_unsigned:
+                        expected_str = f'{expected_value_unsigned:#066x}'  
+                        actual_str = f'{actual_value_unsigned:#066x}'
+                        result.err(f'Mismatch for register {reg}:\n'
+                                    f'  Expected: {expected_str}\n'
+                                    f'  Actual:   {actual_str}')
+
+                else:
+                    if actual_value_unsigned != expected_value_unsigned:
+                        expected_str = f'{expected_value_unsigned:#8x}'
+                        actual_str = f'{actual_value_unsigned:#8x}'
+                        result.err(f'Mismatch for register {reg}:\n'
+                                    f'  Expected: {expected_str}\n'
+                                    f'  Actual:   {actual_str}')
+
+
+    if result.has_errors() or result.has_warnings() or args.verbose:
+        print(result.report())
+
+    return 1 if result.has_errors() else 0
